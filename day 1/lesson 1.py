a611400a53518a719c4b9c96edbfc0e1dcc6d928
temptemo <uunnamed213@gmail.com>
2024-02-24 14:17:25 -0800

    second commit

---
+++ day 1/lesson 1.py
@@ -0,0 +1,5 @@
+print("me gavicani da davumegobrdi jeko tarieladzes")
+
+print("GOA aris sivrce sadac pirvelive wutebidan davinteresdi shemotavazebuli programirebit mzad var axali gamowvevebistvis da shevidzino codna")
+
+
---
+++ day 3/lesson 3 family.py
@@ -0,0 +1,5 @@
+mother = "natia"
+father = "daviti"
+little_sister = "mariam"
+
+print(mother + " " + " " + father + " " + " " + little_sister)
