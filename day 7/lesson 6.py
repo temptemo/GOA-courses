c16cee5751935d9534f9dd4cd995a27d9f2ae6e1
temptemo <uunnamed213@gmail.com>
2024-03-23 11:20:51 -0700

    seventh commit

---
+++ day 7/lesson 6.py
@@ -0,0 +1,33 @@
+#2
+# my_string = input("Write a string: ")
+# my_integer = int(input("Write an integer: "))
+# my_float = float(input("Write a float: "))
+# my_bool = bool(input("Write a Something"))
+
+# the_bool = 56
+# correct_bool = the_bool > my_bool
+
+# print(type(my_string), my_string)
+# print(type(my_integer), my_integer)
+# print(type(my_float), my_float)
+# print(type(my_bool), correct_bool)
+#3
+# a = 10
+# b = 10.0
+# c = "10"
+# d = 10
+# e = 10
+# f = 10
+
+# comparison_result = (a == b == int(c) == d == e == f == 10)
+
+# print(comparison_result)  # This will print True
+#4
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
